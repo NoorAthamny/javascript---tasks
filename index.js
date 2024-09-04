f7e165a4cb3e46cfe62927f5b15c0ec5bb57cabc
NoorAthamny <athamnynoor@gmail.com>
2024-09-04 08:30:33 +0300

    primitive types

---
+++ index.js
@@ -0,0 +1,15 @@
+const myName = "Noor Atamny";
+console.log(myName);
+
+const sum1 = 10;
+const sum2 = 20;
+console.log(sum1 + sum2);
+
+const myBooTrue = true;
+console.log(myBooTrue);
+
+let theUndefined;
+console.log(theUndefined);
+
+let nullValue = null;
+console.log(nullValue);
