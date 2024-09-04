ced3d71cd20ff02dee24b5dce64617bc6f758b1f
NoorAthamny <athamnynoor@gmail.com>
2024-09-04 09:02:37 +0300

    teTemplate literals

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const parisTemp = "18℃";
+const myName = "Noor Athamny";
+const age = 25;
 
-const isLogedIN = true;
+console.log(`My name is ${myName} and i am ${age} years old..`);
 
-const userEmail = "athamnynoor@gmail.com";
+const length = 5;
+const width = 3;
 
-const totalScore = 35;
+console.log(
+  `the area of a rectangle with length of ${length} and width of ${width} is ${
+    length * width
+  }. `
+);
+
+const day = "Monday";
+const task1 = "Learn javascript";
+const task2 = "Read a book";
+const task3 = "try so stay a live.";
+
+console.log(`On ${day} i will ${task1} and ${task2} and ${task3}`);
+
+const myPets = 3;
+
+console.log(`i have ${myPets} pets.`);
+
+const triangleBase = 10;
+const trianleHeight = 5;
+
+console.log(
+  `The area of a the triangle with base of ${triangleBase} and height of ${trianleHeight} is ${
+    (triangleBase * trianleHeight) / 2
+  }`
+);
