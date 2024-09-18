62e2f9850f669281b134e1a7796102ef28a4c9cf
NoorAthamny <athamnynoor@gmail.com>
2024-09-18 21:32:14 +0300

    object - object folder

--- movie-project/objects/object.js
+++ movie-project/objects/object.js
@@ -5,7 +5,7 @@ const movieDatabase = {
     {
       title: "",
       ratings: [],
-      averageRating: [],
+      averageRating: 0,
     },
   ],
   // 1.3 add movie titke
@@ -21,27 +21,35 @@ const movieDatabase = {
   // 1.4 remove movie titke
 
   removeMovie(title) {
-    const remove = {
-      title: title,
-      ratings: [],
-      averageRating: 0,
-    };
-    this.movies.pop(remove);
+    this.movies = this.movies.filter((movie) => movie.title !== title);
   },
   // 2.1 add ratings
-  addRating(title, rating) {
-    for (let mov of this.movies) {
-      if (mov.title === title) {
-        mov.ratings.push(rating);
-
-        return;
-      }
-    }
-
-    console.log(`${title}: IS NOT A MOVIE`);
+  addRatings(title, ratings) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) movie.ratings.push(...ratings);
+    this.calculateAverageRating(title);
   },
   // 2.2 remove rating
-  removeRating(title, rating) {},
+  removeRating(title, ratings) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) {
+      movie.ratings = movie.ratings.filter((r) => r !== ratings);
+    }
+    this.calculateAverageRating(title);
+  },
+  //3.1 Calculating Average Ratings
+  calculateAverageRating(title) {
+    const movie = this.movies.find((movie) => movie.title === title);
+    if (movie) {
+      movie.averageRating =
+        movie.ratings.reduce((acc, curr) => acc + curr) / movie.ratings.length;
+    }
+  },
+  searchMovie(title) {
+    return this.movies.filter((movie) =>
+      movie.title.toLowerCase().includes(title.toLowerCase())
+    );
+  },
 };
 
 movieDatabase.addMovie("Avatar");
@@ -51,14 +59,21 @@ movieDatabase.addMovie("The sad");
 movieDatabase.addMovie("Ninga War");
 
 console.log(movieDatabase);
-
 movieDatabase.removeMovie("Avatar");
 console.log(movieDatabase.movies);
-movieDatabase.addRating("Inception", 9.2);
-movieDatabase.addRating("The Godfather", 10);
-movieDatabase.addRating("The sad", 4.4);
-movieDatabase.addRating("Ninga War", 8.4);
+movieDatabase.addRatings("Inception", [9.2, 8, 7, 4, 2, 8]);
+movieDatabase.addRatings("The Godfather", [10, 8, 5, 4, 9, 8]);
+movieDatabase.addRatings("The sad", [4.4, 5, 6, 7, 4, 6.7]);
+movieDatabase.addRatings("Ninga War", [8.4, 9, 7, 8, 9, 10]);
+console.log(movieDatabase.movies);
 
 // 2. Adding and Removing Ratings
 
 console.log("***************Adding and Removing Ratings************");
+movieDatabase.removeRating("Inception", 8);
+console.log(movieDatabase.movies);
+movieDatabase.calculateAverageRating("Ninga War");
+console.log(movieDatabase.movies);
+console.log(movieDatabase.searchMovie("inception"));
+
+// 5. i will do the sort later cuz i dont realy understand it 100%
