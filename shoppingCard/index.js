022d2af10ee640da04fd7422f99d9168f34ceb06
NoorAthamny <athamnynoor@gmail.com>
2024-09-27 12:19:53 +0300

    update

--- shoppingCard/index.js
+++ shoppingCard/index.js
@@ -1,7 +1,9 @@
 // 1.
 const cart = {};
+// adding
 function addProductToCart(productId) {
   return function () {
+    // Logic for adding product to cart
     if (cart[productId]) {
       cart[productId]++;
     } else {
@@ -12,14 +14,21 @@ function addProductToCart(productId) {
 }
 
 function updateCart() {
+  //store the cart items
+  // clear previuos ent using empty string
   const cartItems = document.querySelector(".cart-items");
   cartItems.innerHTML = "";
 
   for (let [productId, quantity] of Object.entries(cart)) {
+    // Logic goes here...
+
     const cartItemDiv = document.createElement("div");
+    // creating the el
     cartItemDiv.classList.add("cart-item");
+
     cartItemDiv.innerHTML = `Product ID: ${productId}
     Quantity: ${quantity}`;
+
     cartItems.appendChild(cartItemDiv);
   }
 }
